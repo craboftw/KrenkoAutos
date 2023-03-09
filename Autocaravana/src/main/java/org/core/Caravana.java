aad582c031824cb7fdc363d3ddf2985728520934
miriamarmario <75133998+MiriamAC@users.noreply.github.com>
2023-03-09 17:27:07 +0100

    Update Caravana.java

--- Autocaravana/src/main/java/org/core/Caravana.java
+++ Autocaravana/src/main/java/org/core/Caravana.java
@@ -2,5 +2,22 @@ package org.example;
 
 //A class to represent a Car booking system
 public class Caravana {
+    private int id;
+    private String modelo;
+    private float precioPorDia;
+
+    public Caravana (int identificador){ //constructor
+        id = identificador;
+    }
+
+    //observadores
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public float getPrecioPorDia(){
+        return precioPorDia;
+    }
 
 }
