b6edb8de4902a02ba1bcea3553276a88a7090508
craboftw <100842244+craboftw@users.noreply.github.com>
2023-03-23 01:21:00 +0100

    Plantuml creado

--- Autocaravana/src/main/java/org/core/Cliente.java
+++ Autocaravana/src/main/java/org/core/Cliente.java
@@ -206,5 +206,5 @@ public class Cliente {
 //‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧                                        ██        ░░░░░░░░░░          ██
 //Francisco López Guerrero                                  ████                    ████
 //Miriam Armario Cantos                                         ████          ██████
-//‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧                                                  ██████████
+//‧⋆ ✧˚₊‧⋆. ✧˚₊‧⋆‧                                                     ██████████
 
